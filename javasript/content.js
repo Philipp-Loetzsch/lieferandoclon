e9be46126c5ac9b768bf2276673a758a9e8bedc5
Philipp Lötzsch <philipp.loetzsch@outlook.com>
2024-06-24 21:56:13 +0200

    add new content and style footer

--- javasript/content.js
+++ javasript/content.js
@@ -3,91 +3,151 @@ let menus = {
     {
       dish: ["Gemüsesuppe"],
       description: ["Suppe mit frischem Gemüse aus kontrolliertem Anbau"],
-      price: [1.2]
+      price: [2.2]
     },
     {
       dish: ["Spargelcremesuppe"],
       description: ["Suppe aus frischem Spargel"],
-      price: [2.2]
+      price: [4.2]
     },
     {
       dish: ["Kürbissuppe"],
       description: ["Suppe aus frischen Kürbis"],
-      price: [2.8]
+      price: [4.8]
     },
     {
       dish: ["Kartoffelsuppe"],
       description: ["Suppe aus frischen Kartoffeln und Kräutern aus dem Garten"],
-      price: [3.2]
+      price: [4.2]
     },
     {
       dish: ["Hühnersuppe"],
       description: ["Suppe mit feinem Hühnerfleisch und Gemüse der Saison"],
-      price: [4.2]
+      price: [5.2]
     }
   ],
   salats: [
     {
       dish: ["Gemischter Salat"],
       description: ["Blattsalat mit Tomate und Gurke"],
-      price: [2.5]
+      price: [3.5]
     },
     {
       dish: ["Coleslaw"],
       description: ["Original amerikanisch"],
-      price: [1.8]
+      price: [4.8]
     },
     {
       dish: ["Nudelsalat"],
       description: ["Nudelsalat mit Vollkornnudeln, frischem Gemüse und Mayonaise"],
-      price: [2.9]
+      price: [5.9]
     },
     {
       dish: ["Kartoffelsalat"],
       description: ["Omas originaler Kartoffelsalat"],
-      price: [3.8]
-    },
+      price: [5.8]
+    }
   ],
   fishes: [
     {
-      dish: ["Lachsfilet"],
-      description: ["Frisches Lachsfilet in Spinat-Rahmsoße"],
-      price: [6.8]
+      dish: ["Lachsfilet mit Bandnudeln"],
+      description: ["Frisches Lachsfilet in Spinat-Rahmsoße dazu grüne Bandnudeln"],
+      price: [12.8]
+    },
+    {
+      dish: ["BBQ Garnelen"],
+      description: ["Frische Garnelen in Honig-Senf-Soße"],
+      price: [8.4]
+    },
+    {
+      dish: ["Einmarinierter Hering"],
+      description: ["Heringsfilets mit Äpfeln und Zwiebeln"],
+      price: [10.6]
     }
   ],
   beefs: [
     {
       dish: ["Rinderhacksteak"],
       description: ["Gehacktes Rindfleisch mit Kartoffeln, Gemüße und brauner Soße"],
-      price: [5.40]
+      price: [8.4]
+    },
+    {
+      dish: ["Schmorbraten vom Rind"],
+      description: ["Filetstück mit Klößen, Rotkohl und brauner Soße"],
+      price: [13.3]
+    },
+    {
+      dish: ["Rinderrouladen"],
+      description: ["3 Rinderrouladen mit einer Füllung aus Zwiebeln, Gurken, Senf und Speck, in einer köstlichen Rotweinsoße mit Röstgemüse "],
+      price: [12.9]
     }
   ],
   porks: [
     {
       dish: ["Schweinebraten"],
       description: ["Bio-Schweinefleisch mit Kartoffeln und Gemüse"],
-      price: [4.60]
+      price: [9.6]
+    },
+    {
+      dish: ["Geschnetzeltes Schweinefilet"],
+      description: ["Zarte Schweinefilets geschnetzelt in Senfsahne"],
+      price: [13.2]
+    },
+    {
+      dish: ["Pulled Pork"],
+      description: ["zarter Schweinebraten direkt vom Grill"],
+      price: [15.7]
     }
   ],
   noodles: [
     {
       dish: ["Nudeln mit Tomatensoße"],
       description: ["Spirelli mit Tomatensoße und Käse"],
-      price: [1.50]
+      price: [3.5]
+    },
+    {
+      dish: ["Käsespätzle"],
+      description: ["Spätzle in Sahnesoße mit Käse überbacken"],
+      price: [7.9]
+    },
+    {
+      dish: ["Lasagne"],
+      description: ["Klassisch, einfach, gut!"],
+      price: [6.3]
     }
   ],
   desserts: [
     {
       dish: ["Eis"],
-      description: ["3 Kugeln Eis (Vanille, Schokolade und Erdbeere"],
-      price: [2.30]
+      description: ["3 Kugeln Eis (Vanille, Schokolade und Erdbeere)"],
+      price: [2.3]
+    },
+    {
+      dish: ["Cremiges Brownie-Kirsch-Trifle"],
+      description: ["Saftige Brownies kombiniert mit einer leckeren Sahne-Creme und Kirschen."],
+      price: [5.9]
+    },
+    {
+      dish: ["Blätterteig mit Erdbeeren"],
+      description: ["Feine Plunder mit Pudding"],
+      price: [4.6]
     }
   ],
   drinks: [
     {
       dish: ["Cola"],
       description: ["0,5l Flasche Cola"],
-      price: [2.50]
+      price: [4.5]
+    },
+    {
+      dish: ["Gartenlimo"],
+      description: ["0,5l Flasche Gartenlimo (Grapefruit)"],
+      price: [5.2]
+    },
+    {
+      dish: ["Hausgemachte Limonade"],
+      description: ["0,3l Glas mit unserer hausgemachten Limonade"],
+      price: [6.7]
     }
   ]
 };
